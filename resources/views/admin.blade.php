af6ecf74c3f618cfa0dcc397a916b534c79b4437
OKUMURA-env <n.okumura@env-value.co.jp>
2023-09-04 18:44:30 +0900

    トップページを作成

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+    @include('layouts.nav')
+
+    @include('layouts.sidebar')
+
+    @include('layouts.content')
+
+    @include('layouts.controlbar')
+
+    @include('layouts.footer')
+
+@endsection
