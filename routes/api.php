3ced7c1692224a0ec96fcd086dc493f44d376054
OKUMURA-env <n.okumura@env-value.co.jp>
2023-09-04 20:15:07 +0900

    event crudルーティング作成

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('event',App\Http\Controllers\EventController::class)->only(['store','show','update','destroy']);
