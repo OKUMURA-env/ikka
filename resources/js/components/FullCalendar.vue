ea90c34597b0f620135dd9349e1d385d200dd73e
OKUMURA-env <n.okumura@env-value.co.jp>
2023-09-04 19:54:09 +0900

    FullCalendarオプション設定

--- resources/js/components/FullCalendar.vue
+++ resources/js/components/FullCalendar.vue
@@ -3,22 +3,53 @@
 </template>
 
 <script>
-    import FullCalendar from '@fullcalendar/vue3'
-    import dayGridPlugin from '@fullcalendar/daygrid'
-    import interactionPlugin from '@fullcalendar/interaction'
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from "@fullcalendar/list";
 
-    export default {
-        components: {
-            FullCalendar // make the <FullCalendar> tag available
-        },
-        data() {
-            return {
-                calendarOptions: {
-                    plugins: [ dayGridPlugin, interactionPlugin ],
-                    initialView: 'dayGridMonth',
-                    
-                }
+export default {
+    components: {
+        FullCalendar // make the <FullCalendar> tag available
+    },
+    data() {
+        return {
+            calendarOptions: {
+                plugins: [
+                    dayGridPlugin,
+                    timeGridPlugin,
+                    interactionPlugin,
+                    listPlugin,
+                ],
+                initialView: 'dayGridMonth',
+                locale: "ja",
+                firstDay: 1,
+                headerToolbar: {
+                    left: "prev,next,today",
+                    center: "title",
+                    right: "dayGridMonth,timeGridTwoWeek,timeGridDay,listMonth",
+                },
+                buttonText: {
+                    today: '今月',
+                    month: "月",
+                    week: "週",
+                    day: "日",
+                    list: "予定リスト",
+                },
+                views: {
+                    timeGridTwoWeek: {
+                        type: "dayGridWeek",
+                        duration: {
+                            weeks: 2,
+                        },
+                        buttonText: "2週間",
+                    },
+                },
+                weekends: true,
+
             }
         }
     }
+}
 </script>
