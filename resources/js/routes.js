8cc34f9024417a8b836723616f985c28a411873b
OKUMURA-env <n.okumura@env-value.co.jp>
2023-09-04 19:42:24 +0900

    routes.jsにFullCalendar.vueを記載

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,11 +1,17 @@
 import ExampleComponent from './components/Example.vue';
 import { createRouter, createWebHistory } from 'vue-router';
+import FullCalendarComponent from './components/FullCalendar.vue';
 
 const routes = [
     {
         path: "/",
         component: ExampleComponent,
         name:'home',
+    },
+    {
+        path: "/calendar",
+        component: FullCalendarComponent,
+        name:'calendar',
     },
 ];
 
