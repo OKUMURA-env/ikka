260d3fa7c19bf60c8f4fea967295c9475947d12b
OKUMURA-env <n.okumura@env-value.co.jp>
2023-09-05 15:09:02 +0900

    簡易的な新規登録モーダル作成

---
+++ resources/js/components/CreateEventModal.vue
@@ -0,0 +1,113 @@
+<template>
+   <div
+    
+        v-cloak
+        tabindex="-1"
+        aria-labelledby="CreateEventModal"
+        aria-hidden="true"
+        role="dialog"
+        style="display: block"
+        @click.self="closeModal"
+    >
+    <div class="modal-dialog modal-dialog-scrollable">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">新規登録</h5>
+                    <button
+                        type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"
+                        @click="closeModal"
+                        aria-label="Close"
+                    ></button>
+                </div>
+                <div class="modal-body">
+                    <div class="p-2">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">
+                                <div class="input-group input-group-seamless">
+                                    <p>タイトル</p>
+                                    <input
+                                        type="text"
+                                        class="form-control w-100 modal-title"
+                                        placeholder="Add title ..."
+                                        ref="eventTitle"
+                                        autofocus
+                                    />
+                                </div>
+                            </li>
+
+                            <li class="list-group-item">
+                                <div class="input-group input-group-seamless">
+                                    <p>開始</p>
+                                    <input
+                                        type="date"
+                                        class="form-control w-100 modal-title"
+                                        ref="eventStartDate"
+                                        autofocus
+                                    />
+                                </div>
+                            </li>
+
+                            <li class="list-group-item">
+                                <div class="input-group input-group-seamless">
+                                    <p>終了</p>
+                                    <input
+                                        type="date"
+                                        class="form-control w-100 modal-title"
+                                        ref="eventEndDate"
+                                        autofocus
+                                    />
+                                </div>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="modal-footer">
+                    <button
+                        type="button"
+                        class="btn btn-secondary"
+                        @click="closeModal"
+                        data-dismiss="modal"
+                    >
+                        閉じる
+                    </button>
+                    <button
+                        type="button"
+                        class="btn btn-primary"
+                        @click="saveEvent"
+                    >
+                        登録
+                    </button>
+                </div>
+
+            </div>
+        </div>
+    <div v-cloak></div>
+   
+</div>
+</template>
+
+<script>
+
+export default {
+    components: {
+    },
+    data() {
+        
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+
+    },
+
+    
+
+};
+</script>
