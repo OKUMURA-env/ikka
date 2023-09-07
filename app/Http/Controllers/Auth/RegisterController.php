dc76b9fb8120901e4144d353d3b9730ee12ac7fb
OKUMURA-env <n.okumura@env-value.co.jp>
2023-09-07 13:24:52 +0900

    RegisterControllerのregisterメソッド作成

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use \Symfony\Component\HttpFoundation\Response;
+
+class RegisterController extends Controller
+{
+    public function register(Request $request)
+    {
+        /** 
+         * @var Illuminate\Validation\Validator $validator 
+         * 
+         */
+       // バリデーション
+       $validator = Validator::make($request->all(), [
+        'name' => ['required'],
+        'email' => ['required', 'email'],
+        'password' => ['required']
+    ]);
+
+    if ($validator->fails()) {
+        return response()->json($validator->messages(), Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    // ユーザーを作成
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password),
+    ]);
+
+    return response()->json('User registration completed', Response::HTTP_OK);
+    }
+}
