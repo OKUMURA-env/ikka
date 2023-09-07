b27954ccf0ace5aac33b6b5916473a82ce3bea80
OKUMURA-env <n.okumura@env-value.co.jp>
2023-09-07 15:35:35 +0900

    Bearerトークンをリクエストヘッダーに詰める

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,9 +9,10 @@ window._ = _;
 
 import axios from 'axios';
 window.axios = axios;
+axios.defaults.withCredentials = true;	
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-axios.defaults.withCredentials = true;	
+window.axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
