63b57da739b0abfe5c0e0379aa63dc3e59ae8a14
OKUMURA-env <n.okumura@env-value.co.jp>
2023-09-09 11:29:51 +0900

    ヘッダーやサイドバーをbladeファイルからコンポーネントに変更

---
+++ resources/js/components/Layouts/AdminLayout.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="wrapper">
+      <!-- header -->
+      <Header></Header>
+      <!-- /.header -->
+  
+      <!-- sidebar -->
+      <Sidebar></Sidebar>
+      <!-- /.sidebar -->
+  
+      <!-- content -->
+      <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <!-- <ContentHeader></ContentHeader> -->
+        <!-- /.Content Header (Page header) -->
+  
+        <!-- Main content -->
+        <section class="content">
+          <div class="container-fluid">
+            <!-- ここに各ページのコンテンツが表示されます -->
+            <slot></slot>
+          </div>
+        </section>
+        <!-- /.content -->
+      </div>
+      <!-- /.content-wrapper -->
+  
+      <!-- Control sidebar content goes here -->
+      <!-- <ControlSidebar></ControlSidebar> -->
+      <!-- /.Control sidebar content goes here -->
+      
+      <!-- footer -->
+      <!-- <Footer></Footer> -->
+      <!-- /.footer -->
+  
+    </div>
+  </template>
+    
+  <script>
+  import Header from "./Header.vue";
+  import Sidebar from "./Sidebar.vue";
+ 
+  
+  export default {
+    components: {
+      Header,
+      Sidebar,
+      
+    },
+  };
+  </script>
