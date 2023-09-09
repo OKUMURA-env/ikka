89f1e9cd2cc6f2d27887b0846012963c3c60d4a6
OKUMURA-env <n.okumura@env-value.co.jp>
2023-09-09 11:32:52 +0900

    各リクエストのヘッダーにトークンを設定

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,15 @@ import 'admin-lte';
 import router from './routes';
 import { createApp } from 'vue';
 
+// axios のリクエストに Authorization ヘッダーを設定
+axios.interceptors.request.use((config) => {
+    const token = localStorage.getItem("authToken");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+});
+
 const app = createApp({})
 app.use(router)
 app.mount('#app');
