f72a23c959eb69277929323c6134765787a91fa7
OKUMURA-env <n.okumura@env-value.co.jp>
2023-09-09 11:37:54 +0900

    Login.vue修正・処理の記載変更

--- resources/js/components/Auth/Login.vue
+++ resources/js/components/Auth/Login.vue
@@ -1,52 +1,115 @@
 <template>
-    <div>
-        <h2>Login</h2>
-        <form @submit.prevent="login">
-            <div>
-                <label><input v-model="email" placeholder="email"></label>
-            </div>
-            <div>
-                <label><input v-model="password" placeholder="password"></label>
-            </div>  
-            <br>
-            <button type="submit">Login</button>
-        </form>
+    <div class="login-page" style="min-height: 496.797px">
+      <div class="login-box">
+        <div class="login-logo">
+          <a href="../../index2.html"><b>株式会社</b>一荷</a>
+        </div>
+  
+        <div class="card">
+          <div class="card-body login-card-body">
+            <p class="login-box-msg">Sign in to start your session</p>
+            <form @submit.prevent="login">
+              <div class="input-group mb-3">
+                <input 
+                  type="text" 
+                  name="email" 
+                  class="form-control" 
+                  v-model="user.email" 
+                  placeholder="Email..." 
+                  autocomplete="true"/>
+                <div class="input-group-append">
+                  <div class="input-group-text">
+                    <span class="fas fa-id-card"></span>
+                  </div>
+                </div>
+              </div>
+              <div class="input-group mb-3">
+                <input
+                  type="password"
+                  name="password"
+                  v-model="user.password"
+                  class="form-control"
+                  placeholder="Password"
+                  autocomplete="true"
+                />
+                <div class="input-group-append">
+                  <div class="input-group-text">
+                    <span class="fas fa-lock"></span>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-8">
+                  <!-- <div class="icheck-primary">
+                    <input type="checkbox" id="remember" />
+                    <label for="remember"> Remember Me </label>
+                  </div> -->
+                </div>
+  
+                <div class="col-4">
+                  <button type="submit" class="btn btn-primary btn-block">
+                    Login
+                  </button>
+                </div>
+              </div>
+            </form>
+  
+            <!-- <p class="mb-1">
+              <a href="forgot-password.html">I forgot my password</a>
+            </p>
+            <p class="mb-0">
+              <router-link to="register" class="text-center">
+                Register a new membership
+              </router-link>
+            </p> -->
+          </div>
+        </div>
+      </div>
     </div>
-</template>
-<script>
-export default {
-    data() {
+  </template>
+  
+  <script>
+    export default {
+      data() {
         return {
-            email : "",
-            password : "",
-
-        };
-    },
-    methods: {
-        login() {
-            axios.get('/sanctum/csrf-cookie')
-                .then((res) => {
-                    axios.post('/api/login', {
-                        email: this.email,
-                        password: this.password,
-                    })
-                    .then((res) => {
-                        console.log(res.data.token);
-                        if( res.status == 200 ) {
-                            console.log(res.data.token);
-                            localStorage.setItem('token', res.data.token);
-                            this.$router.push("/calendar");
-                            console.log(res.data.token);
-                        }
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                    })
-                })
-                .catch((err) => {
-                //
-                })
+          user: {
+            email: "",
+            password: "",
+          }
         }
+      },
+      methods: {
+        async login() {
+          if(!this.user.password || !this.user.email){
+            alert('ログインIDとパスワードを入力してください。');
+            return;
+          }
+          await axios
+            .get("/sanctum/csrf-cookie");
+          try {
+            await axios
+              .post("/api/login", this.user)
+              .then((response => {
+                console.log(response.data.access_token);
+                  // Save the authToken in localStorage
+                  localStorage.setItem("authToken", response.data.access_token);
+                  console.log(this.user);
+                  this.$router.replace({ name: "calendar" });
+              }))
+          } catch(response) {
+            alert(response.response.data.message);
+          }
+        },
+        // closeControlSidebar() {
+        //   const body = document.querySelector("body");
+        //   if (body.classList.contains("control-sidebar-slide-open")) {
+        //     body.classList.remove("control-sidebar-slide-open");
+        //   }
+        // },
+      },
+      mounted() {
+        // this.closeControlSidebar();
+      }
     }
-};
-</script>
+  </script>
+  
