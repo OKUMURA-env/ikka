9b2bc1180113d133f55d7e110ae669a0f13ac54c
OKUMURA-env <n.okumura@env-value.co.jp>
2023-09-14 20:04:10 +0900

    カテゴリー作成フォームと新規登録メソッドの記載

--- resources/js/components/ScheduleCategories/CreateModal.vue
+++ resources/js/components/ScheduleCategories/CreateModal.vue
@@ -1,18 +1,120 @@
 <template>
-    <h1>テスト</h1>
+    <div
+        v-if="show"
+        class="modal show display"
+        v-cloak
+        tabindex="-1"
+        aria-labelledby="CreateModal"
+        aria-hidden="true"
+        role="dialog"
+        style="display: block"
+        @click.self="closeModal"
+    >
+        <div class="modal-dialog modal-dialog-scrollable">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">カテゴリー登録</h5>
+                    <button
+                        type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"
+                        @click="closeModal"
+                        aria-label="Close"
+                    ></button>
+                </div>
+
+                <div class="modal-body">
+                    <div class="p-2">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">
+                                <div class="input-group input-group-seamless">
+                                    <p>タイトル</p>
+                                    <input
+                                        type="text"
+                                        class="form-control w-100 modal-title"
+                                        v-model="schedule_category.title"
+                                        placeholder="Add title ..."
+                                        ref="scheduleCategoryTitle"
+                                        autofocus
+                                    />
+                                </div>
+                            </li>
+
+                            <li class="list-group-item">
+                                <div class="input-group input-group-seamless">
+                                    <p>カラー</p>
+                                    <input
+                                        type="color"
+                                        class="form-control w-100 modal-title"
+                                        v-model="schedule_category.color"
+                                        ref="scheduleCategoryColor"
+                                        autofocus
+                                    />
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="modal-footer">
+                    <button
+                        type="button"
+                        class="btn btn-secondary"
+                        @click="closeModal"
+                        data-dismiss="modal"
+                    >
+                        閉じる
+                    </button>
+                    <button
+                        type="button"
+                        class="btn btn-primary"
+                        @click="saveScheduleCategory"
+                    >
+                        登録
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <div v-cloak></div>
+    </div>
 </template>
 
 <script>
 import axios from "axios";
 
 export default {
-    props: [],
+    props: ["show"],
     data() {
-        
+        return {
+            schedule_category: {
+                title: null,
+            },
+            users: [],
+        };
     },
 
     methods: {
-        
+        closeModal() {
+            this.schedule_category.title = null;
+            this.schedule_category.color = null;
+            this.$emit("close");
+        },
+        saveScheduleCategory() {
+            axios
+                .post("/api/schedule-categories", {
+                    title: this.schedule_category.title,
+                    color: this.schedule_category.color,
+                })
+                .then(({ data }) => {
+                    this.closeModal();
+                    this.$emit("close");
+                    this.$emit("rerender-schedule-categories");
+                })
+                .catch((error) => {
+                    this.$emit("error");
+                });
+        },
     },
 
 };
