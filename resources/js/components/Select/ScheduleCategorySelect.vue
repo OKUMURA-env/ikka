2016058c35314cd94c69b1829a688b81230011c1
OKUMURA-env <n.okumura@env-value.co.jp>
2023-09-16 15:52:25 +0900

    ScheduleCategorySelect.vue作成

---
+++ resources/js/components/Select/ScheduleCategorySelect.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="input-group input-group-seamless">
+        <p>カテゴリー</p>
+        <select class="form-control" 
+                v-model="selected"
+                @change = "onChange">
+            <option disabled value="nobody">未選択</option>
+            <option
+                v-for="schedule_category in schedule_categories"
+                v-bind:value = "schedule_category.id"
+                v-bind:key = "schedule_category.id"
+            >
+                {{ schedule_category.title }}
+            </option>
+        </select>
+        <div>選択項目: {{ selected }}</div>
+    </div>
+</template>
+
+<script>
+import { stringifyStyle } from "@vue/shared";
+import axios from "axios";
+
+export default {
+    props: ["schedule_categories","scheduleCategoryId"],
+    data() {
+        return {
+            event: {
+                schedule_category_id: "",
+            },
+            selected:this.scheduleCategoryId || 'nobody',
+        };
+    },
+
+    methods: {
+        onChange(event) {
+          this.$emit("change", event);
+         }
+    },
+};
+</script>
