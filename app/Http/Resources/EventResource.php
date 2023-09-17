882d5bff83b247bc948b7737099708a80ab5f7cb
OKUMURA-env <n.okumura@env-value.co.jp>
2023-09-17 12:25:18 +0900

    EventResource.php作成　必要なデータを格納

---
+++ app/Http/Resources/EventResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class EventResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'start' => $this->start,
+            'end' => $this->end,
+            'all_day' => $this->all_day,
+            'schedule_category_id' => $this->schedule_category_id,
+            'driver_id' => $this->driver_id,
+            'driver_name' => $this->driver->name,
+            'schedule_category_title' => $this->scheduleCategory->title,
+        ];
+    }
+}
