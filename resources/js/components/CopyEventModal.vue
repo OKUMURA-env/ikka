cb87e767ffb2fae66dd04e1785355c798b9c4b1c
OKUMURA-env <n.okumura@env-value.co.jp>
2023-09-18 15:25:49 +0900

    copyEventメソッド定義

--- resources/js/components/CopyEventModal.vue
+++ resources/js/components/CopyEventModal.vue
@@ -125,6 +125,7 @@
                    <button
                        type="button"
                        class="btn btn-warning"
+                       @click="copyEvent"
                    >
                        複製
                    </button>
@@ -151,7 +152,6 @@ export default {
    data() {
        return {
            event: {},
-           
        };
    },
 
@@ -186,14 +186,107 @@ export default {
        },
        handleDriverChange(event, display_name) {
            this.driverId = event.target.value;
-
        },
+       copyEvent() {
+            let all_day = this.event.all_day;
+
+            //終日のときにtimeの値がundifinedになるのを防ぐため
+            if(all_day){
+                this.event.start_time = "";
+                this.event.end_time = "";
+            }
+
+            let start = this.event.start_date + " " + this.event.start_time;
+            let end = this.event.end_date + " " + this.event.end_time;
+
+            if (start >= end) {
+                alert("終了日は開始日より後に設定してください。");
+                return;
+            }
+
+            //終日をチェックしてないとき、時間フォームを空のまま登録できないようにする。
+            if(!all_day && this.event.start_time === undefined){
+                alert("終日にチェックするか、時間を指定してください。");
+                return;
+            }
+
+            if(!all_day && this.event.start_time === ""){
+                alert("終日にチェックするか、時間を指定してください。");
+                return;
+            }
+            
+            if(!all_day && this.event.end_time === undefined){
+                alert("終日にチェックするか、時間を指定してください。");
+                return;
+            }
+
+            if(!all_day && this.event.end_time === ""){
+                alert("終日にチェックするか、時間を指定してください。");
+                return;
+            }
+
+            console.log(this.event);
+            axios
+                .post("/api/event", {
+                    title: this.event.title,
+                    start: start,
+                    end: end,
+                    schedule_category_id: this.event.schedule_category_id,
+                    driver_id: this.driverId,
+                    all_day: this.event.all_day,
+                })
+                .then(({ data }) => {
+                    this.closeModal();
+                    this.$emit("event-copied");
+                })
+                .catch((error) => {
+                    this.$emit("error");
+                });
+        },
       
    },
      // 注意！
    // watchを使わないと、eventが編集できない。
    watch: {
-      //
+        currentEvent(currentEvent) {
+                console.log(currentEvent);
+                if (currentEvent.all_day == 0) {
+                    console.log(currentEvent.start.slice(0, 10));
+                    let start_date = currentEvent.start.slice(0, 10);
+                    let start_time = currentEvent.start.slice(11, 16);
+                    let end_date = currentEvent.end.slice(0, 10);
+                    let end_time = currentEvent.end.slice(11, 16);
+                    var Event = {
+                        id: currentEvent.id,
+                        title: currentEvent.title,
+                        start_date: start_date,
+                        start_time: start_time,
+                        end_date: end_date,
+                        end_time: end_time,
+                        driver_id: currentEvent.driver_id,
+                        schedule_category_id: currentEvent.schedule_category_id,
+                        all_day: false,
+                    };
+                }
+                if (currentEvent.all_day == 1) {
+                    var Event = {
+                        id: currentEvent.id,
+                        title: currentEvent.title,
+                        start_date: currentEvent.start,
+                        end_date: currentEvent.end,
+                        driver_id: currentEvent.driver_id,
+                        schedule_category_id: currentEvent.schedule_category_id,
+                        all_day: true,
+                    };
+                }   
+            
+                // ※ JSON.parse(JSON.stringify(currentEvent));
+                // 検索結果のスケジュールからモーダルを起動させた場合、スケジュールに合わせて、
+                // 検索結果上の予定も変更されてしまうため。
+                this.event = JSON.parse(JSON.stringify(Event));
+                this.scheduleCategoryId = currentEvent.schedule_category_id;
+                this.driverId = currentEvent.driver_id;
+            },
    },
 
 
